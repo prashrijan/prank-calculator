f8ae8113cf65bb533d5bbc24d52614d0ec44e849
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-05 14:43:25 +1100

    v0.0.1.1

--- script.js
+++ script.js
@@ -15,6 +15,52 @@ let lastOperator = "";
 let strToDisplay = "";
 const audio = new Audio("./assets/aa.wav");
 
+const calcOperations = (buttonValue) => {
+  if (buttonValue === "AC") {
+    strToDisplay = "";
+    display(strToDisplay);
+    return;
+  }
+
+  let lastChar = strToDisplay[strToDisplay.length - 1];
+
+  // calculate if the lastChar is not an operator
+  if (buttonValue === "=" || buttonValue === "Enter") {
+    if (operations.includes(lastChar)) {
+      strToDisplay = slice();
+    }
+    return calculate();
+  }
+
+  if (buttonValue === "C" || buttonValue === "Backspace") {
+    strToDisplay = slice();
+    return display(strToDisplay);
+  }
+
+  // if the pressed value is operator change the last operator and if the last char is again a operator than slice it
+  if (operations.includes(buttonValue)) {
+    lastOperator = buttonValue;
+    if (operations.includes(lastChar)) {
+      strToDisplay = slice();
+    }
+  }
+
+  if (buttonValue === ".") {
+    // get the last opertor index
+    let lastOperatorIndex = strToDisplay.lastIndexOf(lastOperator);
+
+    // slice the number set from last opertor index to the end
+    let lastNumberSet = strToDisplay.slice(lastOperatorIndex + 1);
+
+    if (lastNumberSet.includes(".")) return;
+
+    if (!lastOperator && strToDisplay.includes(".")) return;
+  }
+
+  strToDisplay += buttonValue;
+  display(strToDisplay);
+};
+
 const display = (str) => {
   input.innerText = str || "0.00";
 };
@@ -22,57 +68,6 @@ const display = (str) => {
 const slice = () => {
   return strToDisplay.slice(0, -1);
 };
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const buttonValue = e.target.innerText;
-
-    input.classList.remove("prank");
-    if (buttonValue === "AC") {
-      strToDisplay = "";
-      display(strToDisplay);
-      return;
-    }
-
-    let lastChar = strToDisplay[strToDisplay.length - 1];
-
-    // calculate if the lastChar is not an operator
-    if (buttonValue === "=") {
-      if (operations.includes(lastChar)) {
-        strToDisplay = slice();
-      }
-      return calculate();
-    }
-
-    if (buttonValue === "C") {
-      strToDisplay = slice();
-      return display(strToDisplay);
-    }
-
-    // if the pressed value is operator change the last operator and if the last char is again a operator than slice it
-    if (operations.includes(buttonValue)) {
-      lastOperator = buttonValue;
-      if (operations.includes(lastChar)) {
-        strToDisplay = slice();
-      }
-    }
-
-    if (buttonValue === ".") {
-      // get the last opertor index
-      let lastOperatorIndex = strToDisplay.lastIndexOf(lastOperator);
-
-      // slice the number set from last opertor index to the end
-      let lastNumberSet = strToDisplay.slice(lastOperatorIndex + 1);
-
-      if (lastNumberSet.includes(".")) return;
-
-      if (!lastOperator && strToDisplay.includes(".")) return;
-    }
-
-    strToDisplay += buttonValue;
-    display(strToDisplay);
-  });
-});
 
 const calculate = () => {
   if (!strToDisplay) return;
@@ -95,7 +90,67 @@ const randomNumber = () => {
 
   return num < 5 ? num : 0;
 };
+
 document.addEventListener("keydown", (e) => {
-  if (e.key === "AC") {
-  }
+  const val = e.key;
+  const ignoredKeys = [
+    "Shift",
+    "Control",
+    "Alt",
+    "Meta",
+    "CapsLock",
+    "Tab",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "Home",
+    "End",
+    "PageUp",
+    "PageDown",
+    "Escape",
+    "Delete",
+    "Insert",
+    "ContextMenu",
+    "F1",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "F6",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "'",
+    '"',
+    ">",
+    "<",
+    "?",
+    "|",
+  ];
+
+  console.log(e);
+
+  if (e.code.includes("Key") || ignoredKeys.includes(val)) return;
+  calcOperations(val);
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const buttonValue = e.target.innerText;
+
+    input.classList.remove("prank");
+    calcOperations(buttonValue);
+  });
 });
