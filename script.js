2b2b81946d790a00cba63b920c2f1ee9a88538ec
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-05 11:50:29 +1100

    v0.0.1 prank calculator

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
+// Create a function to get the value out of the button
+// Add Event Listener to the buttons when clicked to trigger the function
+// Read the value of the button
+// Store all values in global varibale
+// Create a function that will take the value from global variable and displays in the display element
+
+// Select all the buttons
 const buttons = document.querySelectorAll(".button");
-const input = document.querySelector("input");
+const input = document.querySelector(".input");
+
+const operations = ["+", "-", "*", "/", "%"];
+
+let strToDisplay = "";
+
+const display = (str) => {
+  input.innerText = str || "0.00";
+};
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (e.target.textContent === "AC") {
-      input.value = "";
-    } else if (e.target.textContent === "C") {
-      input.value = input.value.slice(0, -1);
-    } else {
-      input.value += e.target.textContent;
+    const buttonValue = e.target.innerText;
+
+    if (buttonValue === "AC") {
+      strToDisplay = "";
+      display(strToDisplay);
+      return;
     }
+
+    if (buttonValue === "=") {
+      const lastChar = strToDisplay[strToDisplay.length - 1];
+
+      if (operations.includes(lastChar)) {
+        strToDisplay = strToDisplay.slice(0, -1);
+      }
+      return calculate();
+    }
+
+    if (buttonValue === "C") {
+      strToDisplay = strToDisplay.slice(0, -1);
+      return display(strToDisplay);
+    }
+
+    let lastChar = strToDisplay[strToDisplay.length - 1];
+
+    if (operations.includes(lastChar)) {
+      if (operations.includes(buttonValue)) {
+        strToDisplay = strToDisplay.slice(0, -1);
+      }
+    }
+
+    strToDisplay += buttonValue;
+    display(strToDisplay);
   });
 });
+
+const calculate = () => {
+  if (!strToDisplay) return;
+
+  const result = eval(strToDisplay);
+  strToDisplay = result.toString();
+  display(result);
+};
