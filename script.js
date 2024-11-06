55d516d262923c4069bc1d1c8f2c50c854056f4f
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-06 14:06:39 +1100

    prank calculator build on won

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const display = document.querySelector(".input");
 let strToDisplay = "";
 let lastOperator = "";
 let operators = ["+", "-", "/", "*", "%"];
-let prankAudio = new Audio("./assets/prank-sound.mp3");
+let prankAudio = new Audio("./assets/prank-sound.mkv");
 
 const appendToDisplay = (str) => {
   return (display.innerText = str || "0.00");
@@ -29,7 +29,7 @@ const calculate = (str) => {
     }
 
     let result = eval(str) + randomVal;
-    strToDisplay = result;
+    strToDisplay = result.toString();
     appendToDisplay(strToDisplay);
     return result;
   } catch (error) {
@@ -43,47 +43,80 @@ const generateRandomNumber = () => {
   return randomNum < 5 ? randomNum : 0;
 };
 
+const allOperations = (val) => {
+  display.classList.remove("prank");
+  9;
+  let lastChar = strToDisplay[strToDisplay.length - 1];
+  if (val === "AC") {
+    clearDisplay();
+    return;
+  }
+
+  if (val === "C" || val == "Backspace") {
+    strToDisplay = removeLastChar(strToDisplay);
+    appendToDisplay(strToDisplay);
+    return;
+  }
+
+  if (operators.includes(lastChar)) {
+    lastOperator = lastChar;
+    if (operators.includes(val)) {
+      strToDisplay = removeLastChar(strToDisplay);
+    }
+  }
+
+  if (val === "=" || val === "Enter") {
+    if (operators.includes(lastChar)) {
+      strToDisplay = removeLastChar(strToDisplay);
+    }
+
+    return calculate(strToDisplay);
+  }
+
+  if (val === ".") {
+    let lastOperatorIndex = strToDisplay.lastIndexOf(lastOperator);
+    let lastNumberSet = strToDisplay.slice(lastOperatorIndex + 1);
+
+    if (lastNumberSet.includes(".")) return;
+
+    if (!lastOperator && strToDisplay.includes(".")) return;
+  }
+  strToDisplay += val;
+  appendToDisplay(strToDisplay);
+};
+
+document.addEventListener("keydown", (e) => {
+  const val = e.key;
+  const calculatorKeys = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    "Enter",
+    "Backspace",
+    ".",
+  ];
+
+  if (!calculatorKeys.includes(val)) return;
+
+  allOperations(val);
+});
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const val = button.innerText;
-    let lastChar = strToDisplay[strToDisplay.length - 1];
-    display.classList.remove("prank");
+    const buttonValue = button.innerText;
 
-    if (val === "AC") {
-      clearDisplay();
-      return;
-    }
-
-    if (val === "C") {
-      strToDisplay = removeLastChar(strToDisplay);
-      appendToDisplay(strToDisplay);
-      return;
-    }
-
-    if (operators.includes(lastChar)) {
-      lastOperator = lastChar;
-      if (operators.includes(val)) {
-        strToDisplay = removeLastChar(strToDisplay);
-      }
-    }
-
-    if (val === "=") {
-      if (operators.includes(lastChar)) {
-        strToDisplay = removeLastChar(strToDisplay);
-      }
-
-      return calculate(strToDisplay);
-    }
-
-    if (val === ".") {
-      let lastOperatorIndex = strToDisplay.lastIndexOf(lastOperator);
-      let lastNumberSet = strToDisplay.slice(lastOperatorIndex + 1);
-
-      if (lastNumberSet.includes(".")) return;
-
-      if (!lastOperator && strToDisplay.includes(".")) return;
-    }
-    strToDisplay += val;
-    appendToDisplay(strToDisplay);
+    allOperations(buttonValue);
   });
 });
